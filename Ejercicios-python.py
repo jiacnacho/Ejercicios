65a3e27576d339c8c972f204eefe49bc842d433d
jiacnacho <53543338+jiacnacho@users.noreply.github.com>
2019-09-09 14:47:31 -0300

    ok

--- Ejercicios-python.py
+++ Ejercicios-python.py
@@ -10,13 +10,23 @@ contador=0
 countneg=0
 numeros=0
 countpos=0
-for i in range(1,10,1):
-    numeros=input('Ingrese un numero')
+multiquince=0
+cuentamultiquince=0
+sumapares=0
+cuentapares=0
+
+for i in range(1,11,1):
+    numeros=int(input('Ingrese un numero'))
     if (numeros<=0):
         countneg+=1
     elif (numeros >=0):
         countpos+=1
-
+    if ((numeros%15)==0):
+        cuentamultiquince+=1;
+    if ((numeros%2)==0):
+        cuentapares=cuentapares+numeros
+    
 print(u"Hay", countneg, "numeros negativos.")
 print(u"Hay", countpos, "numeros positivos.")
-
+print(u"Hay", cuentamultiquince, "numeros multiplos de 15")
+print(u"La suma de los nros pares es de:", cuentapares, )
